19a05b1f4e2a82ba13799088e599d298e9823732
Jean Lucca <jjeanlucca@outlook.com>
2026-07-21 16:51:22 -0300

    sdfsdkj

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-    }),
     react(),
+    tailwindcss(),
   ],
-  resolve: {
-    tsconfigPaths: true // A nova forma nativa que o Vite pediu
-  }
-});
+})

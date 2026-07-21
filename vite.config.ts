fc0c75fc01cf5b5d3241be91e4069c79c9aabae7
Jean Lucca <jjeanlucca@outlook.com>
2026-07-21 14:49:56 -0300

    fix: atualiza config do vite e força geração das rotas

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    // Este é o plugin que gera o routeTree.gen.ts automaticamente
     TanStackRouterVite({
       routesDirectory: "./src/routes",
       generatedRouteTree: "./src/routeTree.gen.ts",
     }),
     react(),
-    tsconfigPaths(), // Resolve os aliases como "@/" automaticamente
   ],
+  resolve: {
+    tsconfigPaths: true // A nova forma nativa que o Vite pediu
+  }
 });

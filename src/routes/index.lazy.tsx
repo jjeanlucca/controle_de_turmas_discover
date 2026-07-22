795f635b801c642c1b7f11d09326ca68b616ce43
Jean Lucca <jjeanlucca@outlook.com>
2026-07-22 11:14:55 -0300

    sklfsk

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 
-export const Route = createLazyFileRoute('/turmas')({
+export const Route = createLazyFileRoute('/')({
   component: DashboardPage,
 })
 
